b928342fed9ddca121700b61dac417c5b183ad85
seanbarton <seanbarton@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-11 11:25:25 +0000

    updated for V1.5. added new functionality in the changelog
    git-svn-id: https://plugins.svn.wordpress.org/test-gateway-for-woocommerce/trunk@1218094 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -5,17 +5,17 @@ Tested up to: 4.4.*
 Stable tag: trunk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Tags: WooCommerce, Payment gateway, test gateway
+Tags: WooCommerce, Payment gateway, test gateway, paymentless gateway
 
-Adds a test payment gateway to WooCommerce available only to logged in administrators or when WP_DEBUG is enabled
+A payment gateway plugin for Woocommerce to handle test or paymentless transactions. Shows for admin only by default, everyone in WP_DEBUG or using a variety of gateway settings
 
 == Description ==
 
-Adds a test payment gateway to WooCommerce available only to logged in administrators or when WP_DEBUG is enabled. If you enable this and then look inside your available payment processors for WooCommerce you will see the basic settings for the plugin.
+Adds a test payment gateway to WooCommerce. By default it is available only to logged in administrators or when WP_DEBUG is enabled. However, there is a setting which enables the gateway for everyone to use. This is especially useful where you want customers to enter their address info etc but for no money to change hands. Similar to the COD or BACS gateways except this will set their order as paid instead of pending. If you enable this and then look inside your available payment processors for WooCommerce you will see the basic settings for the plugin.
 
-This can be safelty used in the live environment for telephone/email orders etc as only an administrator can see the gateway. I've been using it for testing conversion code from a variety of different affiliate systems.
+This can be safely used in the live environment for telephone/email orders etc as outside of the settings to enable for certain groups, only an administrator can see the gateway. I've been using it for testing conversion code from a variety of different affiliate systems.
 
-Also when WP_DEBUG is set to true in your wp-config.php file this will show for everyone. Not recommended in the live environment but is useful when testing the user journey for new builds.
+Also when WP_DEBUG is set to true in your wp-config.php file this will show for everyone. Very useful when testing the user journey for new builds or in situations where payment is not needed
 
 == Installation ==
 
@@ -28,3 +28,7 @@ Also when WP_DEBUG is set to true in your wp-config.php file this will show for 
 = 1.4 =
 * Initial build. Has been used for a few versions but never version controlled
 * Updates to follow if requested!
+
+= 1.5 =
+* Added 'enable for visitors' option in the settings
+* Added 'enable for IP' option in the settings
--- test_gateway_for_woocommerce.php
+++ test_gateway_for_woocommerce.php
@@ -3,10 +3,10 @@
 /**
  * Plugin Name: Test Payment Module for Woocommerce
  * Plugin URI:  http://www.tortoise-it.co.uk
- * Description: A payment gateway plugin for Woocommerce to handle test payments. Shows for admin only
+ * Description: A payment gateway plugin for Woocommerce to handle test or paymentless transactions. Shows for admin only by default or everyone in WP_DEBUG or using a gateway setting
  * Author:      Sean Barton (Tortoise IT)
  * Author URI:  http://www.tortoise-it.co.uk
- * Version:     1.4
+ * Version:     1.5
  */
 
 function sb_wc_test_init() {
@@ -19,10 +19,10 @@ function sb_wc_test_init() {
 		public function __construct() {
 			$this->id = 'sb_test';
 			$this->has_fields = false;
-			$this->method_title = __( 'Test', 'woocommerce' );
+			$this->method_title = __( 'Test/Paymentless', 'woocommerce' );
 			$this->init_form_fields();
 			$this->init_settings();
-			$this->title = 'Test gateway';
+			$this->title = 'Test/Paymentless gateway';
 	
 			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
 		}
@@ -34,13 +34,25 @@ function sb_wc_test_init() {
 					'type' => 'checkbox',
 					'label' => __( 'Enable test gateway', 'woocommerce' ),
 					'default' => 'yes'
+				),
+				'enabled_visitors' => array(
+					'title' => __( 'Enable for visitors', 'woocommerce' ),
+					'type' => 'checkbox',
+					'label' => __( 'Allow non admins to use this gateway (for testing or for paymentless stores)', 'woocommerce' ),
+					'default' => 'no'
+				),
+				'enabled_ip' => array(
+					'title' => __( 'Enable for specific IP addresses', 'woocommerce' ),
+					'type' => 'textarea',
+					'label' => __( 'In the following field enter IP addresses (one per line) to enable this gateway for specific IPs', 'woocommerce' ),
+					'default' => ''
 				)
 			);
 		}
 	    
 		
 		public function admin_options() {
-			echo '	<h3>Test gateway</h3>
+			echo '	<h3>Test/Paymentless gateway</h3>
 				<table class="form-table">';
 				
 			$this->generate_settings_html();
@@ -66,7 +78,27 @@ function sb_wc_test_init() {
 	}	
 
 	function add_sb_test_gateway( $methods ) {
-		if (current_user_can('administrator') || WP_DEBUG) {
+		
+		$show_visitors = $show_ip = false;
+		
+		if ($settings = get_option('woocommerce_sb_test_settings')) {
+			if (isset($settings['enabled_visitors']) && $settings['enabled_visitors'] == 'yes') {
+				$show_visitors = true;
+			}
+			if (isset($settings['enabled_ip'])) {
+				if ($ips = explode("\n", $settings['enabled_ip'])) {
+					foreach ($ips as $ip) {
+						$ip = trim($ip);
+						if ($_SERVER['REMOTE_ADDR'] == $ip) {
+							$show_ip = true;
+							break;
+						}
+					}
+				}
+			}
+		}
+		
+		if (current_user_can('administrator') || WP_DEBUG || $show_visitors || $show_ip) {
 			$methods[] = 'WC_Gateway_sb_test';
 		}
 		
